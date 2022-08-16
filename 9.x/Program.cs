45719116b8c78b8e44656b0a03942a18274778f2
Exreller <pipkonp@mail.ru>
2022-08-16 12:50:44 +0300

    Добавьте файлы проекта.

---
+++ 9.x/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9.x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
