5979dfd12dd176d6f2d17d32ca84070c4a940e42
Exreller <pipkonp@mail.ru>
2022-08-21 16:51:07 +0300

    Удалил вынесенный класс персон

--- 9.x/Program.cs
+++ 9.x/Program.cs
@@ -3,52 +3,133 @@ using System.Collections.Generic;
 
 namespace DelegatePractices
 {
-    
-    
-    class Program
+    public class Person
     {
-        
-        public void SortName(List<_9.x.Person> personList, int sortType)
+        public string Name { get; set; }
+
+    }
+
+    class Myexception : Exception 
+    {
+        public Myexception(string message) 
+            :base(message)
         {
 
         }
+    }
 
+    class InputReader
+    {
+
+        public int NumberRead()
+        {
+            
+            Console.WriteLine("Для сортировки А-Я введите 1\nДля соритовки Я-А введите 2");
+                        
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number != 1 && number != 2)
+            {
+                throw new Myexception("Неверный ввод");
+            }
+            else
+            {
+                return number;
+            }
+                
+        }
+               
+    }
+
+    class Sort
+    {
+        public delegate void SortDelegate(List<Person> personList);
+        public event SortDelegate SortEvent;
+
+        public void SortName(List<Person> personList, int sortType)
+        {
+            switch (sortType)
+            {
+                case 1:
+                    {
+                        personList.Sort();
+                        break;
+                    }
+                case 2:
+                    {
+                        personList.Reverse();
+                        break;
+                    }
+            }
+
+            EnteredSortType(personList);
+
+        }
+                
+        protected virtual void EnteredSortType(List<Person> personList)
+        {
+            SortEvent?.Invoke(personList);
+        }
+    }
+    class Program
+    {
+        
         static void Main(string[] args)
         {
 
-            _9.x.Person person1 = new()
+            Person person1 = new()
             {
                 Name = "Александр"
             };
 
-            _9.x.Person person2 = new()
+            Person person2 = new()
             {
                 Name = "Николай"
             };
 
-            _9.x.Person person3 = new()
+            Person person3 = new()
             {
                 Name = "Веньямин"
             };
 
-            _9.x.Person person4 = new()
+            Person person4 = new()
             {
                 Name = "Феофан"
             };
 
-            _9.x.Person person5 = new()
+            Person person5 = new()
             {
                 Name = "Людвиг"
             };
 
-            List<_9.x.Person> PersonList = new();
+            List<Person> PersonList = new();
             PersonList.Add(person1);
             PersonList.Add(person2);
             PersonList.Add(person3);
             PersonList.Add(person4);
             PersonList.Add(person5);
 
+            InputReader inputReader = new();
+            Sort sort = new();
+            sort.SortEvent += ShowSortedPerson;
 
+            try
+            {
+                sort.SortName(PersonList, inputReader.NumberRead());
+            }
+            catch (Myexception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введены не цифры");
+            }
+
+            static void ShowSortedPerson(List<Person> personList)
+            {
+                foreach(var person in personList)
+                Console.WriteLine(personList);
+            }
         }
     }
 }
