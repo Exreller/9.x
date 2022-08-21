008e5fd02be6a6ba1e2987e1942ee6377f6aeef9
Exreller <pipkonp@mail.ru>
2022-08-21 14:08:06 +0300

    Вынес класс пёрсн

--- 9.x/Program.cs
+++ 9.x/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DelegatePractices
 {
+    
+    
     class Program
     {
         
+        public void SortName(List<_9.x.Person> personList, int sortType)
+        {
+
+        }
+
         static void Main(string[] args)
         {
-           
+
+            _9.x.Person person1 = new()
+            {
+                Name = "Александр"
+            };
+
+            _9.x.Person person2 = new()
+            {
+                Name = "Николай"
+            };
+
+            _9.x.Person person3 = new()
+            {
+                Name = "Веньямин"
+            };
+
+            _9.x.Person person4 = new()
+            {
+                Name = "Феофан"
+            };
+
+            _9.x.Person person5 = new()
+            {
+                Name = "Людвиг"
+            };
+
+            List<_9.x.Person> PersonList = new();
+            PersonList.Add(person1);
+            PersonList.Add(person2);
+            PersonList.Add(person3);
+            PersonList.Add(person4);
+            PersonList.Add(person5);
+
+
         }
     }
 }
